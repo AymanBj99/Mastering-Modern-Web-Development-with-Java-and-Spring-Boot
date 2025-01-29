e781de235cd7d6119492d1b5d77c81dd9e407557
AymanBj99 <ayman.bouajaji66@gmail.com>
2025-01-29 14:16:16 +0100

    add mapping annotations like post get put delete

--- src/main/java/com/packt/football/PlayerController.java
+++ src/main/java/com/packt/football/PlayerController.java
@@ -8,5 +8,27 @@ public class PlayerController {
     public List<String> listPlayers() {
         return List.of("Ivana ANDRES", "Alexia PUTELLAS");
     }
+
+    @PostMapping
+    public String createPlayer(@RequestBody String name) {
+    return "Player " + name + " created";
+}
+
+    @GetMapping("/{name}")
+    public String readPlayer(@PathVariable String name) {
+    return name;
+}
+
+@DeleteMapping("/{name}")
+public String deletePlayer(@PathVariable String name) {
+return "Player " + name + " deleted";
+}
+
+//Implement a PUT request to update a player:
+@PutMapping("/{name}")
+public String updatePlayer(@PathVariable String name, @RequestBody String
+newName) {
+return "Player " + name + " updated to " + newName;
+}
 }
 
